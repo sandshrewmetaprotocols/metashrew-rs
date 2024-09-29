96a26b92ef94ce4cd9205768d57d1cf418795402
sandshrewmetadev <inquiries@sandshrew.io>
2024-09-29 18:52:14 +0000

    copy vec to ptr

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 pub fn ptr_to_vec(ptr: i32) -> Vec<u8> {
   unsafe {
     let len = *((ptr - 4) as usize as *const usize);
-    Vec::<u8>::from_raw_parts(ptr as usize as *mut u8, len, len)
+    Box::leak(Box::new(Vec::<u8>::from_raw_parts(ptr as usize as *mut u8, len, len))).clone()
   }
 }
